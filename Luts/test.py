6e5cdb9fbc31e2aa17d54a5193c5adb83d661614
luta-wolf <lutawolf77@gmail.com>
2020-07-21 13:04:31 +0300

    add False and True

--- Luts/test.py
+++ Luts/test.py
@@ -1,15 +1,7 @@
-import pickle
-
-FILENAME = "user.dat"
-
-name = "Tom"
-age = 19
-
-with open(FILENAME, "wb") as file:
-    pickle.dump(name, file)
-    pickle.dump(age, file)
-
-with open(FILENAME, "rb") as file:
-    name = pickle.load(file)
-    age = pickle.load(file)
-    print("Имя:", name, "\tВозраст:", age)
+t = (4, 5, 6)
+lst = list(t)
+lst[0] = 1
+print(t)
+print(lst)
+tpl = tuple(lst)
+print(tpl)
